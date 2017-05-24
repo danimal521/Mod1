66d4d8c33d86ff01cac31a7872709a2e196b6faf
BuildTools <unconfigured@null.spigotmc.org>
2017-05-23 20:11:57 -0600

    these are my changes !!!

--- src/main/java/com/example/examplemod/ExampleMod.java
+++ src/main/java/com/example/examplemod/ExampleMod.java
@@ -23,8 +23,16 @@ public class ExampleMod //extends BaseMod
         //GameRegistry.addRecipe(new ItemStack(Items.DIAMOND, 2), new Object[] { "DDD", "DDD", "DDD", 'D', Blocks.DIRT});
         
         
-        GameRegistry.addRecipe(new ItemStack(Blocks.FARMLAND, 9), new Object[] { "DDD", "DHD", "DDD", 'D', Blocks.DIRT, 'H', Items.WOODEN_HOE});
+        GameRegistry.addRecipe(new ItemStack(Blocks.FARMLAND, 100), new Object[] { "DDD", "DHD", "DDD", 'D', Blocks.DIRT, 'H', Items.WOODEN_HOE});
         
+	    GameRegistry.addRecipe(new ItemStack(Items.STONE_AXE, 1), new Object[] { "AAA", "A A", "AAA", 'A', Items.WOODEN_AXE});
+	    
+	    GameRegistry.addRecipe(new ItemStack(Blocks.SPONGE, 8), new Object[] { "OOO", "OWO", "OOO", 'O', Blocks.WOOL, 'W', Items.WATER_BUCKET});
+	    
+	    GameRegistry.addRecipe(new ItemStack(Items.GOLDEN_AXE, 1), new Object[] { "AAA", "A A", "AAA", 'A', Items.STONE_AXE});
+	    
+	    GameRegistry.addRecipe(new ItemStack(Items.DIAMOND_AXE, 1), new Object[] { "AAA", "A A", "AAA", 'A', Items.GOLDEN_AXE});
+
         System.out.println("Loaded!");
         
     }
